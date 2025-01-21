7cbbfe62bfd7da0f45fbe318dfd1de928431f691
samithreddy2005 <samithreddy950@gmail.com>
2025-01-21 20:21:33 +0530

    style: add login route to application routing

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,10 +3,12 @@ import Home from "../pages/Home/Home";
 import EventList from "../pages/EventList/EventList";
 import FilterEvents from "../pages/FilterEvents/FilterEvents";
 import EventDetail from "../pages/EventDetails/EventDetails";
+import Login from "../pages/Login";
 
 export const routes = [
   { path: '/', element: <Home /> },
   { path: '/events', element: <EventList /> },
   { path: '/find-events', element: <FilterEvents /> },
-  { path: '/events/:id', element: <EventDetail /> }
+  { path: '/events/:id', element: <EventDetail /> },
+  { path: '/login', element: <Login /> }
 ];
