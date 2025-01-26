3a3973a14d47f600603f7f8bcfc924fdf1682371
Varshith8868 <konalavasrshith@gmail.com>
2025-01-26 13:20:32 +0530

    commits

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,9 +11,14 @@ const Login = () => {
   // Array of background images
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const backgroundImages = [
-    "https://images.pexels.com/photos/2325447/pexels-photo-2325447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1920",
-    "https://images.pexels.com/photos/1983046/pexels-photo-1983046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    //"https://images.pexels.com/photos/2325447/pexels-photo-2325447.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    //"https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&q=80&w=1920",
+    "https://static.vecteezy.com/system/resources/previews/029/332/550/non_2x/ai-generative-party-scene-from-a-festive-night-club-with-happy-people-and-friends-sony-a7s-realistic-image-free-photo.jpg",
+    "https://www.visionvivaah.com/blog/wp-content/uploads/2019/11/Top-10-Event-Management-Companies-In-India.jpg",
+    "https://convene.com/wp-content/uploads/2019/07/Convene-Hackathons-1-scaled.jpg",
+    "https://uploads.lebonbon.fr/source/2023/april/2043806/acb5d4c2-febf-451c-9a64-7c042769fade_1_2000.jpg",
+    //"https://images.pexels.com/photos/1983046/pexels-photo-1983046.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   ];
 
   // Change background image every 3 seconds
