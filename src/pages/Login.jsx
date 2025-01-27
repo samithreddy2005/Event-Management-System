2070e5aa31dcf9088da9f39a85fc07d4b033ce32
Varshith8868 <konalavasrshith@gmail.com>
2025-01-27 20:33:05 +0530

    comits

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,7 +43,7 @@ const Login = () => {
 
   return (
     <div
-      className="login-container"
+      className=" login-container "
       style={{
         backgroundImage: `url(${backgroundImages[currentImageIndex]})`,
       }}
