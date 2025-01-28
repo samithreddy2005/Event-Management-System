8cd51f300d43d0b509c9d81347031eda1979818b
Varshith8868 <konalavasrshith@gmail.com>
2025-01-28 20:32:08 +0530

    commits

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -55,7 +55,7 @@ const Login = () => {
             type="text"
             placeholder="Username"
             value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            onChange={(e) =>  setUsername(e.target.value)}
             required
           />
           <input
