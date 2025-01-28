23ee5af2dad2971dc5447eac560aa098ed91de7e
samithreddy2005 <samithreddy950@gmail.com>
2025-01-29 00:45:16 +0530

    feat: conditionally render Login link based on current route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import {routes} from "./routes/routes"
-import "./App.css"
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Event from './pages/Event';
 
-const router = createBrowserRouter(routes)
-function App() {
+const App = () => {
   return (
-    <>
-      <RouterProvider router={router}/>
-    </>
-  )
-}
+    <Router>
+      <Navigation />
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/event" component={Event} />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
+export default App;
