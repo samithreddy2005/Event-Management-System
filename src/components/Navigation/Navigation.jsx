8d2350a64ece69cf20eb241988dcc3c85a293d38
samithreddy2005 <samithreddy950@gmail.com>
2025-01-29 22:51:55 +0530

    Add Font Awesome icons and enhance navigation animations

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './Navigation.css';
 
+import 'font-awesome/css/font-awesome.min.css'; 
+
 const Navigation = () => {
   const location = useLocation();
 
   return (
     <nav className="navbar">
       <ul className="nav-links">
-        <li className="nav-item">
-          <Link to="/" className="nav-link">Home</Link>
+      <li>
+          <Link to="/"><i className="fa fa-home"></i> </Link> {/* Home icon */}
         </li>
+                {/* Other navigation links */}
         {location.pathname === '/' && (
           <li className="nav-item">
             <Link to="/login" className="nav-link">Login</Link>
